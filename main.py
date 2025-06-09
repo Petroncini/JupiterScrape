a4e20474b95754c2b42ab9ede0074a7454e841f4
Petroncini <caiopetroncini@gmail.com>
2025-06-09 20:18:57 -0300

    and the Word was Beautiful Soup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+
 import time
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")  # Run headless (no GUI)
+options.add_argument("--disable-gpu")  # Disable GPU acceleration (Windows)
+options.add_argument("--no-sandbox")  # Useful for Linux
+options.add_argument("--disable-dev-shm-usage")  # Prevents crashes in headless mode
 
+driver = webdriver.Chrome(options=options)
 driver.get("https://uspdigital.usp.br/jupiterweb/jupCarreira.jsp?codmnu=8275")
 
 wait = WebDriverWait(driver, timeout=2)
@@ -16,6 +27,8 @@ unidadeSelect_element = driver.find_element("id", value="comboUnidade")
 buscarButton = driver.find_element("id", value="enviar")
 
 unidadeSelect = Select(unidadeSelect_element)
+t0 = time.time()
+
 for unidade in unidadeSelect.options[1:]:
     print(unidade.text)
     unidadeSelect.select_by_visible_text(unidade.text)
@@ -27,10 +40,35 @@ for unidade in unidadeSelect.options[1:]:
         print("\t", curso.text)
         cursoSelect.select_by_visible_text(curso.text)
         buscarButton.click()
-        wait.until(lambda _: driver.find_element("id", "step1-tab").is_displayed())
-        abaMenus = driver.find_element("id", value="step1-tab")
-        abaMenus.click()
+
+        try:
+            wait.until(EC.element_to_be_clickable(("id", "step4-tab")))
+            abaGrade = driver.find_element("id", "step4-tab")
+            abaGrade.click()
+
+            wait.until(EC.element_to_be_clickable(("id", "step1-tab")))
+            abaMenus = driver.find_element("id", value="step1-tab")
+            abaMenus.click()
+        except ElementClickInterceptedException:
+            print("Erro - dados não encontrados")
+
+            try:
+                fechar = driver.find_element(By.XPATH, '//button[contains(@class, "ui-button") and .//span[text()="Fechar"]]')
+                fechar.click()
 
 
+                wait.until(EC.element_to_be_clickable(("id", "step4-tab")))
+                abaGrade = driver.find_element("id", "step4-tab")
+                abaGrade.click()
+
+                wait.until(EC.element_to_be_clickable(("id", "step1-tab")))
+                abaMenus = driver.find_element("id", value="step1-tab")
+                abaMenus.click()
+            except NoSuchElementException:
+                print("Fatal")
+
+t1 = time.time()
+total = t1 - t0
+print(f'Time: {total}')
 
 input("Aperte Enter para terminar...")
